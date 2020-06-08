23de703678763cf2e929c9707f16b5f5d2f578fd
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-06-08 15:35:08 -0400

    add image uploader

--- src/components/Pallets/Pallets.jsx
+++ src/components/Pallets/Pallets.jsx
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import ColorPeg from '../ColorPeg/ColorPeg'
 import { SketchPicker, SwatchesPicker, MaterialPicker } from 'react-color';
 import './Pallets.css'
+import ImageUploader from 'react-images-upload';
+import { ColorExtractor } from 'react-color-extractor'
+
 
 class Pallets extends Component{
-    state = {
-        background: '#f35c87',
+    constructor(props) {
+        super(props);
+        this.state = {
+             pictures: '',
+             background: '#f35c87',
         colors : ['#f35c87', '#20639b', '#3caea3', '#f6d55c', '#ed553b', '#17bfd1'],
         id : 0
+             };
+        this.onDrop = this.onDrop.bind(this);
+    }
+
+
+    onDrop(pictureFiles, pictureDataURLs) {
+        this.setState({
+            pictures: pictureDataURLs
+        });
+        
     }
     
     handleChange = (color) => {
@@ -35,13 +51,26 @@ class Pallets extends Component{
                             height='100%'
                             
                     ></SwatchesPicker>
+                    
                     </section>
-                    <div className='Pallets' onClick={this.handlePickColor}>
-                        {this.state.colors.map((color, index) => {
-                            return <ColorPeg color={color} key={index} id={index} onChangen={ this.handleChange}>
-                            </ColorPeg>
-                        })}
+                    <div className='sections'>
+                        <div className='Pallets' onClick={this.handlePickColor}>
+                            {this.state.colors.map((color, index) => {
+                                return <ColorPeg color={color} key={index} id={index} onChangen={ this.handleChange}>
+                                </ColorPeg>
+                            })}
+                        </div>
+                        <ImageUploader
+                        className='uploader'
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+            <img src={this.state.pictures}></img>
                     </div>
+                    
                     <section >
                     <SketchPicker
                             color={ this.state.background }

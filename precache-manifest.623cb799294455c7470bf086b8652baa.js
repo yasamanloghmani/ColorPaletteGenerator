5036e8c97204ab71f67bc5a51656eeadb5c10a2a
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-06-08 12:52:21 -0400

    Updates

---
+++ precache-manifest.623cb799294455c7470bf086b8652baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbefbf7d845e436d4d1725dfb06c8437",
+    "url": "/ColorPaletteGenerator/index.html"
+  },
+  {
+    "revision": "5a85dfaa7b1a1b9663ab",
+    "url": "/ColorPaletteGenerator/static/css/main.c7ae1280.chunk.css"
+  },
+  {
+    "revision": "69e769eb453a2d648646",
+    "url": "/ColorPaletteGenerator/static/js/2.151c23e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ColorPaletteGenerator/static/js/2.151c23e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a85dfaa7b1a1b9663ab",
+    "url": "/ColorPaletteGenerator/static/js/main.7dd0a9b8.chunk.js"
+  },
+  {
+    "revision": "1370fc96ecae13d3f30d",
+    "url": "/ColorPaletteGenerator/static/js/runtime-main.9f671192.js"
+  }
+]);
